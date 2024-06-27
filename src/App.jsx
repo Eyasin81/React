e9cb8fe9dd2330de802313f2572dad22c616d880
Eyasin81 <eyasina81@gmail.com>
2024-06-27 09:33:34 +0600

    Simple Home Page Add

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 
 import { BrowserRouter,Routes,Route} from 'react-router-dom';
 import './Assets/bootstrap.min.css'
+import Home from './Pages/Home';
 import Expression from './Pages/4_JSX and JS Expression';
 const App = () => {
   return (
    <BrowserRouter>
    <Routes>
    <Route path='/four' element={<Expression/>}/>
+   <Route path='/' element={<Home/>}/>
    </Routes>
    </BrowserRouter>
   );
