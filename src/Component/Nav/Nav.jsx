1705ce69b766e72ce7db60d6062abeda395e65bb
Eyasin81 <eyasina81@gmail.com>
2024-07-03 16:24:37 +0600

    Css Add Navbar Color Chang

--- src/Component/Nav/Nav.jsx
+++ src/Component/Nav/Nav.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import './Nav.css'
 import {NavLink} from 'react-router-dom'
+import { IoHomeOutline } from "react-icons/io5";
 const Nav = () => {
    return (
     <nav>
-    <NavLink to='/' className='nav-link'>Home</NavLink>
+    <NavLink to='/' className='nav-link'><IoHomeOutline/></NavLink>
     <NavLink to='/four' className='nav-link'>Expression</NavLink>
     <NavLink to='/useState' className='nav-link'>UseState</NavLink>
     </nav>
